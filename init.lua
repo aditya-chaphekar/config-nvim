b26733dab390d00a57f1b15a0979337b6c6a33a2
Aditya <adityachaps@gmail.com>
2024-04-22 14:07:44 +0530

    ftr: minor changes

--- init.lua
+++ init.lua
@@ -516,13 +516,18 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'olimorris/onedarkpro.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    lazy = false,
+    priority = 1000,
+    opts = { transparency = true },
+    config = function()
+      require('onedarkpro').setup {
+        options = {
+          transparency = true,
+        },
+      }
+    end,
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
@@ -532,9 +537,6 @@ require('lazy').setup({
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
-    opts = {
-      transparency = true,
-    },
   },
 
   -- Highlight todo, notes, etc in comments
